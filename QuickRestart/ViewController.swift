309e8699f8e31539347c05407192ab2326202f69
nowperson <imajinkawabe@gmail.com>
2019-06-29 14:41:48 +0900

    Initial Commit

---
+++ QuickRestart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickRestart
+//
+//  Created by Imagine Kawabe on 2019/06/29.
+//  Copyright © 2019 Imagine Kawabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
